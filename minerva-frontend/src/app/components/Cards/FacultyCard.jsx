03d296a09bca6768fb99e8d7862bc8c5ff993d64
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-12 17:19:24 +0530

    created faclty page

--- minerva-frontend/src/app/components/Cards/FacultyCard.jsx
+++ minerva-frontend/src/app/components/Cards/FacultyCard.jsx
@@ -1,14 +1,18 @@
 export default function FacultyCard({ data }) {
   return (
-    <div className="relative p-3 w-[50vw] md:w-[25vw] lg:w-[19vw] bg-[#800080]">
-      <img className="w-[100%]" src="/image.png" alt="" />
-      <div className="bg-[#800080] text-white tracking-wide m-1 p-2 absolute bottom-0 left-0 w-min text-nowrap flex flex-col items-center">
-        <span className="font-teko text-[4.1vw] md:text-[3vw] lg:text-[1.7vw]">
-          {data.hod}
-        </span>
-        <span className="font-teko text-[3.4vw] md:text-[2.5vw] lg:text-[1.2vw]">
-          {data.hod}
-        </span>
+    <div>
+      <div className="relative p-3 w-[50vw] md:w-[25vw] lg:w-[19vw] bg-[#800080]">
+        <img className="w-[100%]" src="/image.png" alt="" />
+        <div className="bg-[#800080] text-white tracking-wide m-1 p-2 absolute bottom-0 left-0 w-min text-nowrap flex flex-col items-center">
+          <span className="font-teko text-[4.1vw] md:text-[3vw] lg:text-[1.7vw]">
+            {data.name}
+          </span>
+          {data.role && (
+            <span className="font-teko text-[3.4vw] md:text-[2.5vw] lg:text-[1.2vw]">
+              {data.role}
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
--- minerva-frontend/src/app/faculty/page.js
+++ minerva-frontend/src/app/faculty/page.js
@@ -21,19 +21,20 @@ export default function Home() {
         <h1 className="font-saira font-extrabold text-[12vw] md:text-[10vw] lg:text-[8vw] text-[#800080]">
           FACULTY
         </h1>
-        <div className="flex flex-col-reverse md:flex-row justify-around items-center">
-          <div className="relative p-3 w-[50vw] md:w-[25vw] lg:w-[19vw] bg-[#800080]">
-            <FacultyCard data={hod} />
-          </div>
+        <div className="flex flex-col-reverse md:flex-row justify-around">
+          <FacultyCard data={hod} />
           <h2 className="font-saira font-extrabold text-[11vw] md:text-[9vw] lg:text-[7vw]">
             HEAD OF
             <br /> DEPARTMENT
           </h2>
         </div>
+
+        <div></div>
+
         <h2 className="my-8 font-saira font-extrabold text-[10vw] md:text-[8vw] lg:text-[6vw]">
           FACULTY
         </h2>
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {dummy.map((faculty, idx) => (
             <FacultyCard key={idx} data={faculty} />
           ))}
