00a8c9862f2d191c0fdbf338d5914b84ba529eca
devadathkartha <devadathkartha@gmail.com>
2024-12-12 21:29:51 +0530

    changes more changes?

--- minerva-frontend/src/app/components/heropage/Hero.jsx
+++ minerva-frontend/src/app/components/heropage/Hero.jsx
@@ -4,21 +4,21 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className='w-[100vw] h-[100vh] m-0 p-0 box-border'>
-        <div className='relative container-fluid w-[100%] h-[100%] flex flex-col text-center justify-center items-center bg-cover bg-center' style={{backgroundImage:"url('./Hero/hero-bg.png')"}}>
-            <p className='text-white font-saira text-5xl lg:text-6xl font-extrabold w-[65%] p-auto'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</p>
-            <div className='bg-[#FFFBF2] w-[60%] md:w-[45%] rounded-2xl relative top-12 md:top-36' >
-                <div className='flex justify-around p-3 w-[100%] text-[#812981] font-teko text-left'>
-                    <div className='flex flex-col justify-center w-[33%]'>
+        <div className='relative container-fluid w-[100%] h-[100%] flex flex-col text-center justify-center items-center bg-cover bg-center overflow-hidden pt-5' style={{backgroundImage:"url('./Hero/hero-bg.png')"}}>
+            <p className='text-white font-saira text-4xl md:text-5xl lg:text-6xl font-extrabold w-[65%] p-auto'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</p>
+            <div className='bg-[#FFFBF2] min-[400px]:w-[40%] w-[45%] md:w-[60%] rounded-2xl p-5 md:mb-4 relative top-12 md:top-36' >
+                <div className='flex flex-col md:flex-row justify-around md:p-5 w-[100%] text-[#812981] font-teko md:text-left'>
+                    <div className='flex flex-col justify-center md:w-[33%] border-b md:border-b-0 md:border-r-4 border-[#DEBDDD] md:mr-3 mt-3 pb-3 md:pb-0 md:pr-3'>
                         <a className='text-xl md:text-3xl font-bold'>NEWS</a>
-                        <p className='text-black text-lg w-[80%]'>The quick brown fox and he likes carrots</p>
+                        <p className='text-[#696969] text-left text-md md:text-lg md:w-[85%]'>The quick brown fox and he likes carrots</p>
                     </div>
-                    <div className='flex flex-col justify-center w-[33%] '>
+                    <div className='flex flex-col justify-center md:w-[33%] border-b md:border-b-0 md:border-r-4 border-[#DEBDDD] md:mr-3 mt-3 pb-3 md:pb-0 md:pr-3'>
                         <a className='text-xl md:text-3xl font-bold'>UPDATES</a>
-                        <p className='text-black text-lg w-[80%]'>The quick brown fox and he likes carrots</p>
+                        <p className='text-[#696969] text-left text-md md:text-lg md:w-[85%]'>The quick brown fox and he likes carrots</p>
                     </div>
-                    <div className='flex flex-col justify-center w-[33%]'>
+                    <div className='flex flex-col justify-center md:w-[33%] md:mr-3 mt-3 pb-3 md:pb-0 md:pr-3'>
                         <a className='text-xl md:text-3xl font-bold'>EVENTS</a>
-                        <p className='text-black text-lg w-[80%]'>The quick brown fox and he likes carrots</p>
+                        <p className='text-[#696969] text-left text-md md:text-lg md:w-[85%]'>The quick brown fox and he likes carrots</p>
                     </div>
                 </div>
 
--- minerva-frontend/src/app/page.js
+++ minerva-frontend/src/app/page.js
@@ -3,6 +3,8 @@ import Hero from "./components/heropage/Hero";
 
 export default function Home() {
   return (
-    <Hero/>
+    <>
+      <Hero/>
+    </>
   );
 }
