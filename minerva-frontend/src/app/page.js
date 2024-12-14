3248e35e0c3b6d5de7ed9b912fac765b613e9955
devadathkartha <devadathkartha@gmail.com>
2024-12-15 01:11:33 +0530

    props

--- minerva-frontend/src/app/page.js
+++ minerva-frontend/src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
+
 import Hero from "./components/heropage/Hero";
+import Data from "./data.json";
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
+      <Hero props={Data}/>
       <div className="text-5xl">h</div>
     </div>
   );
