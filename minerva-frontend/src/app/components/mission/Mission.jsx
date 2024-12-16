3e2b9d879c9b63e2e8765e1b64bd7174c0a16728
Rithvik <rithvikreddym2006@gmail.com>
2024-12-16 10:51:27 +0530

    Fixed padding

--- minerva-frontend/src/app/components/mission/Mission.jsx
+++ minerva-frontend/src/app/components/mission/Mission.jsx
@@ -1,15 +1,16 @@
 export default function Mission() {
     return (
-        <div className="relative overflow-hidden overflow-y-hidden pb-[5rem] font-saira">
+        <div className="relative overflow-hidden overflow-y-hidden pb-[2rem] md:pb-[4rem] font-saira">
             <div className="bg-[#800080] h-[100%] sm:w-[2.5rem] w-[1.2rem] absolute"></div>
-            <div className="flex items-center justify-end">
-                <div className="text-center text-[#800080]  lg:text-[11.5rem] xl-lg:text-[12rem] md:text-[10rem] sm:text-[8rem] text-[6rem]  w-[100%] sm:h-[10.75rem] flex lg:justify-end justify-center items-center font-bold md:mr-[2rem] lg:mr-[5rem] xl:mr-[10rem]">
+
+            <div className="flex justify-center items-center">
+                <div className="text-center text-[#800080] md:w-[90%]  lg:text-[11.5rem] xl-lg:text-[12rem] md:text-[10rem] sm:text-[8rem] text-[5rem]  w-[100%] sm:h-[10.75rem] flex lg:justify-end justify-center items-center font-bold  lg:mr-[10rem] xl:mr-[10rem]">
                     MISSION
                 </div>
             </div>
 
-            <div className="text-black text-justify font-mont font-bold w-[90%] lg:w-[80%] flex justify-start items-center xl:text-[1.7rem] md:text-[1.5rem] sm:text-[1.3rem] text-[1.1rem] md:ml-[3rem] sm:ml-[1.1rem] ml-[0.7rem]">
-                <p className="w-[90%] md:w-[75%] md:ml-[3.2rem] sm:ml-[3.5rem] ml-[2rem]">
+            <div className="text-black font-mont w-[89%] font-bold text-justify md:mr-[2rem] lg:mr-[0rem] flex justify-center items-center xl:text-[1.7rem] md:text-[1.5rem] sm:text-[1.3rem] text-[1.1rem] md:ml-[3rem] sm:ml-[3rem] ml-[1.6rem] mt-10 mb-10">
+                <p className="w-[90%]">
                     To develop human resource with sound knowledge- theory and
                     practical - in the discipline of computing and the ability
                     to apply the knowledge to the benefit of the society at
--- minerva-frontend/src/app/page.js
+++ minerva-frontend/src/app/page.js
@@ -7,23 +7,20 @@ import Mission from "./components/mission/Mission";
 import History from "./components/history/History";
 import McaProgramme from "./components/mca_programme/mcaProgramme";
 
-
 export default function Home() {
-  return (
-    <div className="overflow-hidden">
-<img src="landing.png" className="w-full"></img>
-<img src="landing.png" className="w-full"></img>
-<img src="landing.png" className="w-full"></img>
-    <div>
-        <Sidebar />
-            <main className="m-0 p-0 bg-white">
-                {/* <h1 className="text-black">Test</h1> */}
-                <Profile />
-                <Vision />
-                <Mission />
-                <History />
-                <McaProgramme />
-            </main>
-    </div>
-  );
+    return (
+        <div className="overflow-hidden">
+            <div>
+                <Sidebar />
+                <main className="m-0 p-0 bg-white">
+                    {/* <h1 className="text-black">Test</h1> */}
+                    <Profile />
+                    <Vision />
+                    <Mission />
+                    <History />
+                    <McaProgramme />
+                </main>
+            </div>
+        </div>
+    );
 }
