009e59f9e4a6545e16d6b9275fc582b29908ec93
Rohith <rohithofficial10@gmail.com>
2024-12-26 15:04:41 +0530

    test

--- minerva-frontend/src/app/labs/page.jsx
+++ minerva-frontend/src/app/labs/page.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Carousel from '../components/image-carousel/swiper.jsx'
 import Card from "../components/lab_card/card.jsx"
+import ImgCard from "../components/imagehero/Imagehero.jsx"
 const images = [{"img":"https://swiperjs.com/demos/images/nature-1.jpg","tag":"Software Systems Labortary"},{"img":"https://swiperjs.com/demos/images/nature-2.jpg","tag":"Goofy Systems Laboratory"},{"img":"https://swiperjs.com/demos/images/nature-3.jpg","tag":"Skibidi Laboratory"}]
 const page = () => {
   return (
     <>
-    <div className='h-[40vh]'>white space</div>
-    <div className='w-[100%] flex justify-center items-center border-2 border-black h-[420px] sm:h-[460px] md:h-[460px]'>
+    <div className='h-[100vh]'>
+      <ImgCard title={"nigga"} />
+    </div>
+    <div className='w-[100%] flex justify-center items-cente h-[420px] sm:h-[460px] md:h-[460px]'>
         <Carousel flag={1} images={images} width={"w-[300px] sm:w-[700px] md:w-[800px]"}/>
     </div>
     <Card />
