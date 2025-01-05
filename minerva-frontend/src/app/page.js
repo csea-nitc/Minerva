3809accc790933527c59a24c2e8f66104144d73f
arunnats <arunnats2004@gmail.com>
2025-01-05 22:21:03 +0530

    [code] added landing code

--- minerva-frontend/src/app/page.js
+++ minerva-frontend/src/app/page.js
@@ -5,29 +5,27 @@ import Mission from "./components/mission/Mission";
 import History from "./components/history/History";
 import McaProgramme from "./components/mca_programme/mcaProgramme";
 import Hero from "./components/heropage/Hero";
-import Data from "./data.json";import Navbar from "./components/navbar/Navbar";
+import Data from "./data.json";
+import Navbar from "./components/navbar/Navbar";
 import Herosupport from "./components/herosupport/Herosupport";
-
 
 export default function Home() {
   return (
     <div>
-
       <Hero props={Data} />
-      <Herosupport/>
-       <div className="overflow-hidden">
-            <div>
-                <Sidebar />
-                <main className="m-0 p-0 bg-white">
-                    {/* <h1 className="text-black">Test</h1> */}
-                    <Profile />
-                    <Vision />
-                    <Mission />
-                    <History />
-                    <McaProgramme />
-                </main>
-            </div>
+      <Herosupport props={Data} />
+      <div className=" w-[100vw]  relative z-10 bg-white">
+        <div className=" mx-auto">
+          <main className="m-0 p-0 bg-white">
+            {/* <h1 className="text-black">Test</h1> */}
+            <Profile />
+            <Vision />
+            <Mission />
+            <History />
+            <McaProgramme />
+          </main>
         </div>
+      </div>
     </div>
   );
 }
