1bf5e1a8f7eb75f1879dd52f26b4d830e1bb2370
Rithvik <rithvikreddym2006@gmail.com>
2025-01-06 01:51:52 +0530

    made the changes on homepage

--- minerva-frontend/src/app/components/heropage/Hero.jsx
+++ minerva-frontend/src/app/components/heropage/Hero.jsx
@@ -1,30 +1,54 @@
+"use client";
 import React from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Hero = ({ props }) => {
-  return (
-    <div className="w-[100%] h-[100vh] m-0 p-0 box-border">
-      <div
-        className="relative container-fluid w-[100%] h-[100%] flex flex-col text-center justify-center items-center bg-cover bg-center pt-5"
-        style={{ backgroundImage: "url('./Hero/hero-bg.png')" }}
-      >
-        <div className="absolute w-[70%] flex flex-col justify-center items-center -translate-y-5">
-          {/* DEPARTMENT OF aligned to the left */}
-          <p className="text-white font-saira text-[1.6vw] font-extrabold absolute left-10 -top-2">
-            DEPARTMENT OF
-          </p>
-          {/* COMPUTER SCIENCE AND ENGINEERING centered */}
-          <p className="text-white font-saira text-[4vw] font-extrabold text-center">
-            COMPUTER SCIENCE AND ENGINEERING
-          </p>
-          {/* NATIONAL INSTITUTE OF TECHNOLOGY, CALICUT aligned to the right */}
-          <p className="text-white font-saira text-[1.6vw] font-extrabold absolute right-8 -bottom-2">
-            NATIONAL INSTITUTE OF TECHNOLOGY, CALICUT
-          </p>
+    const [ismobile, setIsMobile] = useState(false);
+
+    const handleResize = () => {
+        if (window.innerWidth <= 640) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    return (
+        <div className="w-[100%] h-[100vh] m-0 p-0 box-border fixed top-0">
+            <div
+                className="relative container-fluid w-[100%] h-[100%] flex flex-col text-center justify-center items-center bg-cover bg-center pt-5"
+                style={{ backgroundImage: "url('./Hero/hero-bg.png')" }}
+            >
+                <div className="absolute flex flex-col justify-center items-center -translate-y-5">
+                    {/* DEPARTMENT OF aligned to the left */}
+                    <p className="text-white font-saira lg-xl:text-[2rem] lg:text-[1.8rem] md:text-[1.5rem] sm:text-[1.3rem] text-[1.1rem]  font-extrabold text-left w-full leading-tight">
+                        DEPARTMENT OF
+                    </p>
+                    {/* COMPUTER SCIENCE AND ENGINEERING centered */}
+                    <p className="text-white text-justify font-saira lg-xl:text-[4rem] lg:text-[3rem] md:text-[2.7rem] sm:text-[2.2rem] text-[2.5rem] font-extrabold text-center w-full leading-tight">
+                        COMPUTER SCIENCE{" "}
+                        {ismobile && (
+                            <span className="block -m-8">
+                                <br />
+                            </span>
+                        )}{" "}
+                        AND ENGINEERING
+                    </p>
+                    {/* NATIONAL INSTITUTE OF TECHNOLOGY, CALICUT aligned to the right */}
+                    <p className="text-white font-saira lg-xl:text-[2rem] lg:text-[1.8rem] md:text-[1.5rem] sm:text-[1.3rem] text-[1.1rem]  font-extrabold text-right w-full leading-tight">
+                        NATIONAL INSTITUTE OF TECHNOLOGY, CALICUT
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Hero;
