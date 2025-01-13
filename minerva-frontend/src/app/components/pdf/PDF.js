d53757ab68553781b823a03559f7c46da52f14ef
Hafeez-hm <hafeezhid@gmail.com>
2025-01-13 17:07:08 +0530

    changes to pdf comp

--- minerva-frontend/src/app/components/pdf/PDF.js
+++ minerva-frontend/src/app/components/pdf/PDF.js
@@ -2,27 +2,28 @@ import Link from "next/link";
 
 export default function PDF({ title, url }) {
   return (
-    <div className="w-full px-3 sm:px-4 py-2 sm:py-3 
-      rounded bg-[#DEBDDD] 
-      flex justify-between items-center 
-      gap-3 
-      transition-all duration-200 
-      hover:bg-opacity-90">
-      <p className="font-jakarta text-base sm:text-lg flex-1 
-        truncate pr-2" 
-        style={{ wordSpacing: "4px" }}>
-        {title}
-      </p>
-      <Link 
-        href={url} 
-        target="_blank"
-        className="flex-shrink-0 transition-transform duration-200 hover:scale-110">
+    <Link 
+      href={url} 
+      target="_blank" 
+      className="w-full px-3 sm:px-4 py-2 sm:py-3 
+        rounded bg-[#DEBDDD] 
+        flex justify-between items-center 
+        gap-3 
+        transition-all duration-2=100 
+        hover:bg-opacity-90 "
+    >
+      <div className="flex justify-between items-center w-full">
+        <p className="font-jakarta text-base sm:text-lg flex-1 
+          truncate pr-2" 
+          style={{ wordSpacing: "4px" }}>
+          {title}
+        </p>
         <img 
           src="/pdf-icon.svg" 
-          className="h-6 sm:h-8 w-auto" 
+          className="h-6 sm:h-8 w-auto flex-shrink-0 transition-transform duration-200 hover:scale-110" 
           alt="PDF Download"
         />
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
