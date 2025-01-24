c57b086cba92661583ec87e281b51863b816ba82
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-01-24 16:38:19 +0530

    created footer

--- minerva-frontend/src/app/components/footer/Footer.jsx
+++ minerva-frontend/src/app/components/footer/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className="px-[3%] py-[1%] bg-[#800080] font-verdana text-white z-10 relative">
-        <div className="pb-2 flex flex-col md:flex-row justify-around items-center gap-6 border-b border-b-white ">
-          <div>
+        <div className="flex flex-col md:flex-row justify-around items-end gap-6 border-b border-b-white ">
+          <div className="flex flex-col gap-16">
             <div className="px-2 md:px-4 flex items-center gap-6 translate-x-2 md:-translate-x-8 md:translate-y-4">
               <img className="h-[96px] w-[81px]" src="/nitc-logo.png" alt="" />
               <div className="flex flex-col gap-2">
@@ -19,11 +19,12 @@ export default function Footer() {
               </div>
             </div>
             <img
-              className="-translate-x-7 w-[800px] hidden md:block"
-              src="/csea-temp-footer.png"
+              className="w-[800px] hidden md:block"
+              src="/footer.svg"
               alt=""
             />
           </div>
+
           <div className="max-w-[400px] flex-grow">
             <div className="flex flex-col text-nowrap text-[4vw] sm:[3vw] md:text-[2vw] lg:[1.4vw] xl:text-[.7vw]">
               <p className="px-2 py-4 border-b border-white"></p>
