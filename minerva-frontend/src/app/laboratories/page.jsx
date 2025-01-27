b380eb9c81454fffb000c3145c99586e0d93287e
Rithvik <rithvikreddym2006@gmail.com>
2025-01-27 23:15:22 +0530

    Better UI/UX

--- minerva-frontend/src/app/laboratories/page.jsx
+++ minerva-frontend/src/app/laboratories/page.jsx
@@ -1,41 +1,40 @@
-"use client"
+"use client";
 import React from "react";
 import Card from "../components/lab_card/card.jsx";
 import ImgCard from "../components/imagehero/Imagehero.jsx";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
+import Loading from "../components/loading/loading";
 
 const token = process.env.NEXT_PUBLIC_TOKEN;
 const backend_url = process.env.NEXT_PUBLIC_API_URL;
 
 const page = () => {
     const [data, setData] = useState(null);
-    
+
     useEffect(() => {
-    const fetchData = async () => {
-        try {
-        const data = await fetch(
-            `${backend_url}/api/labs?populate=*`,
-            {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+        const fetchData = async () => {
+            try {
+                const data = await fetch(`${backend_url}/api/labs?populate=*`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const Data = await data.json();
+
+                setData(Data.data);
+            } catch (err) {
+                console.error("Fetch error:", err);
             }
-        );
-        const Data = await data.json();
-    
-        setData(Data.data);
-        
-        } catch (err) {
-        console.error("Fetch error:", err);
-        }
-    };
-    fetchData();
+        };
+        fetchData();
     }, []);
 
-    const images = data && data.map(item => ({
-        img: `${backend_url}${item.image.url}`,
-        tag: item.name,
-    }));
+    const images =
+        data &&
+        data.map((item) => ({
+            img: `${backend_url}${item.image.url}`,
+            tag: item.name,
+        }));
 
     return (
         <>
@@ -48,18 +47,23 @@ const page = () => {
             <div className="w-full mt-[40vh] sm:mt-[50vh] md:mt-[60vh] lg:mt-[70vh] relative z-10 bg-white">
                 <div className="bg-[#800080] h-[100%] w-[10px] absolute"></div>
                 <div className="sm:w-[65%] w-[85%] mx-auto py-10">
-                <div className="grid grid-cols-1 gap-y-6">
-                    {data && data.map((item) => ( 
-                        <Card
-                            key={item.id}
-                            title={item.name || "-"}
-                            id={item.documentId || "-"}
-                            faculty={item.faculty || "-"}
-                            staff={item.staff || "-"}
-                            location={item.location || "-"}
-                        />
-                    ))}
-                </div>
+                    {data ? (
+                        <div className="grid grid-cols-1 gap-y-6">
+                            {data &&
+                                data.map((item) => (
+                                    <Card
+                                        key={item.id}
+                                        title={item.name || "-"}
+                                        id={item.documentId || "-"}
+                                        faculty={item.faculty || "-"}
+                                        staff={item.staff || "-"}
+                                        location={item.location || "-"}
+                                    />
+                                ))}
+                        </div>
+                    ) : (
+                        <Loading />
+                    )}
                 </div>
             </div>
         </>
--- minerva-frontend/src/app/research/page.js
+++ minerva-frontend/src/app/research/page.js
@@ -5,15 +5,16 @@ import ImageHero from "../components/imagehero/Imagehero";
 import TabNav from "../components/tabnav/TabNav";
 import Link from "next/link";
 import ListComp from "../components/newscomp/ListComp";
+import Loading from "../components/loading/loading";
 
 const TabData = ["Areas Of Research", "Research Groups", "Completed PhDs"];
 
-const backend_url = process.env.NEXT_PUBLIC_API_URL ; 
-const token  = process.env.NEXT_PUBLIC_TOKEN ; 
+const backend_url = process.env.NEXT_PUBLIC_API_URL;
+const token = process.env.NEXT_PUBLIC_TOKEN;
 
 export default function Programmes() {
     const [selectedTab, setSelectedTab] = useState(0);
-    const [data , setData ] = useState( [] ) ; 
+    const [data, setData] = useState([]);
 
     const [issmall, setIsSmall] = useState(false);
 
@@ -25,28 +26,25 @@ export default function Programmes() {
         window.addEventListener("resize", checkSmall);
         return () => window.removeEventListener("resize", checkSmall);
     }, []);
-    
-    useEffect( ( ) => {
+
+    useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await fetch(
-                    `${backend_url}/api/completed-ph-d`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    }
-                );
-    
+                const data = await fetch(`${backend_url}/api/completed-ph-d`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
                 const Data = await data.json();
-                setData( Data.data ? Data.data : [] );
+                setData(Data.data ? Data.data : []);
             } catch (err) {
                 console.error("Fetch error:", err);
             }
         };
         fetchData();
-    }); 
-    
+    });
+
     return (
         <>
             <ImageHero
@@ -59,63 +57,87 @@ export default function Programmes() {
                 <div className="bg-[#800080] h-[100%] w-[10px] absolute"></div>
                 <div className="sm:w-[65%] w-[85%] mx-auto py-10">
                     {/* Tab Navigation */}
-                    <TabNav tabData={TabData} onTabChange={setSelectedTab} />
+                    {data && data.length > 0 ? (
+                        <>
+                            <TabNav
+                                tabData={TabData}
+                                onTabChange={setSelectedTab}
+                            />
 
-                    <div className="flex flex-col max-1240:pr-[2vw] mb-[10vh]">
-                        {/* Content Rendering */}
-                        <div className="font-jakarta mt-12">
-                            {TabData[selectedTab] === "Areas Of Research" ? (
-                                // Content for Areas Of Research
-                                <div>
-                                    {tabData[selectedTab].titlesAndAreas.map(
-                                        (item, index) => (
-                                            <div key={index} className="mb-6">
-                                                <h3 className="text-2xl font-bold text-[#800080]">
-                                                    {item.title}
-                                                </h3>
-                                                <p className="text-[1.2em] text-gray-700">
-                                                    {item.area}
-                                                </p>
-                                            </div>
-                                        )
+                            <div className="flex flex-col max-1240:pr-[2vw] mb-[10vh]">
+                                {/* Content Rendering */}
+                                <div className="font-jakarta mt-12">
+                                    {TabData[selectedTab] ===
+                                    "Areas Of Research" ? (
+                                        // Content for Areas Of Research
+                                        <div>
+                                            {tabData[
+                                                selectedTab
+                                            ].titlesAndAreas.map(
+                                                (item, index) => (
+                                                    <div
+                                                        key={index}
+                                                        className="mb-6"
+                                                    >
+                                                        <h3 className="text-2xl font-bold text-[#800080]">
+                                                            {item.title}
+                                                        </h3>
+                                                        <p className="text-[1.2em] text-gray-700">
+                                                            {item.area}
+                                                        </p>
+                                                    </div>
+                                                )
+                                            )}
+                                        </div>
+                                    ) : TabData[selectedTab] ===
+                                      "Research Groups" ? (
+                                        // Content for Research Groups
+                                        <div>
+                                            <p className="text-[1.6em] leading-[35px] text-justify">
+                                                Following are the upcoming
+                                                research groups in the
+                                                Department:
+                                            </p>
+                                            <ul className="mt-4 list-disc pl-8 text-[1.6em] text-gray-700">
+                                                {tabData[
+                                                    selectedTab
+                                                ].groups.map((group, index) => (
+                                                    <li
+                                                        key={index}
+                                                        className="mb-2"
+                                                    >
+                                                        {group.link ? (
+                                                            <Link
+                                                                href={
+                                                                    group.link
+                                                                }
+                                                                target="_blank"
+                                                                rel="noopener noreferrer"
+                                                                className="text-[#800080] hover:underline"
+                                                            >
+                                                                {group.name}
+                                                            </Link>
+                                                        ) : (
+                                                            <span>
+                                                                {group.name}
+                                                            </span>
+                                                        )}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    ) : (
+                                        // Content for Completed PhDs
+                                        <div>
+                                            <ListComp item={data} />
+                                        </div>
                                     )}
                                 </div>
-                            ) : TabData[selectedTab] === "Research Groups" ? (
-                                // Content for Research Groups
-                                <div>
-                                    <p className="text-[1.6em] leading-[35px] text-justify">
-                                        Following are the upcoming research
-                                        groups in the Department:
-                                    </p>
-                                    <ul className="mt-4 list-disc pl-8 text-[1.6em] text-gray-700">
-                                        {tabData[selectedTab].groups.map(
-                                            (group, index) => (
-                                                <li key={index} className="mb-2">
-                                                    {group.link ? (
-                                                        <Link
-                                                            href={group.link}
-                                                            target="_blank"
-                                                            rel="noopener noreferrer"
-                                                            className="text-[#800080] hover:underline"
-                                                        >
-                                                            {group.name}
-                                                        </Link>
-                                                    ) : (
-                                                        <span>{group.name}</span>
-                                                    )}
-                                                </li>
-                                            )
-                                        )}
-                                    </ul>
-                                </div>
-                            ) : (
-                                // Content for Completed PhDs
-                                <div>
-                                    <ListComp item = {data}/>
-                                </div>
-                            )}
-                        </div>
-                    </div>
+                            </div>
+                        </>
+                    ) : (
+                        <Loading />
+                    )}
                 </div>
             </div>
         </>
